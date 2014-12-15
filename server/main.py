45fa1a070111854bd823927757ff33a5c95b2fa1
unknown <popcorncolonel@gmail.com>
2014-12-14 22:01:36 -0500

    sick error checking

--- server/main.py
+++ server/main.py
@@ -12,30 +12,35 @@ class API(webapp2.RequestHandler):
         self.response.headers['Access-Control-Allow-Origin'] = '*'
         self.response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
         self.response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT'
-        ####Error checking and input validation####
-        zipcode = self.request.get('zip', None)
-        lat = ''
-        lng = ''
 
         def write(response):
             self.response.write(json.dumps(response, separators=(',',':')))
 
+        ####Error checking and input validation####
+        zipcode = self.request.get('zip', None)
+        
+        lat = self.request.get('lat', None)
+        lng = self.request.get('lng', None)
+
         response = {}
-        try:
+        try: #what gorgeous error checking (not)
             if not (lat and lng) and not zipcode:
                 raise TypeError
-            if not zipcode:
-                lat = self.request.get('lat', None)
-                lng = self.request.get('lng', None)
-                lat = float(lat)
-                lng = float(lng)
-                if lat < -90 or lat > 90
-                    raise TypeError
-                if lng < -180 or lng > 180
-                    raise TypeError
-            else: 
+            if zipcode:
                 zipcode = int(zipcode)
+
+                #if they give like "2155" rather than "02155"
+                if zipcode < 10000 and self.request.get('zip', None)[0] != '0':
+                    raise TypeError
                 if zipcode > 99999 or zipcode < 1000:
+                    raise TypeError
+            if lat:
+                lat = float(lat)
+                if lat < -90 or lat > 90:
+                    raise TypeError
+            if lng:
+                lng = float(lng)
+                if lng < -180 or lng > 180:
                     raise TypeError
         except TypeError:
             logging.warn('Invalid API request - given zip: %s, lat: %s, lng: %s' %(zipcode,lat,lng))
