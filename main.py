567e7f4a37ebf03e19d4f17a81a77518ff93e236
unknown <popcorncolonel@gmail.com>
2014-12-17 19:31:52 -0500

    way way more data

--- main.py
+++ main.py
@@ -30,53 +30,67 @@ class API(webapp2.RequestHandler):
 
         #TODO: VERIFY THIS VERIFY THIS VERIFY THIS PLEEEAAASSSEEEE**************************************************** XXX
         # tri-hourly times are 
-            # 0: 12am-3am
-            # 1: 3am-6am
-            # 2: 6am-9am
-            # 3: 9am-12pm
-            # 4: 12pm-3pm
-            # 5: 3pm-6pm
-            # 6: 6pm-9pm
-            # 7: 9pm-12am
+            # 0: 2am-5am
+            # 1: 5am-8am
+            # 2: 8am-11am
+            # 3: 11am-2pm
+            # 4: 2pm-5pm
+            # 5: 5pm-8pm
+            # 6: 8pm-11pm
+            # 7: 11pm-2am
 
-        def get_avg(data, param, day_night='day'):
+        def get_avg(data, param, night=False):
             avg = 0.0
-            if day_night == 'day':
+            if night:
                 for i in range(4): #4*3 = 12. things are measured in 3hr periods, 12hrs is half the day.
-                    avg += float(data['data']['weather'][0]['hourly'][i+2][param]) # 2, 3, 4, 5
+                    avg += float(data['data']['weather'][0]['hourly'][(i+5) % 8][param]) # 5, 6, 7, 0
                 avg /= 4.0
                 return avg
-            elif day_night == 'night':
+            else:
                 for i in range(4): #4*3 = 12. things are measured in 3hr periods, 12hrs is half the day.
-                    avg += float(data['data']['weather'][0]['hourly'][(i+6) % 8][param]) # 6, 7, 0, 1
+                    avg += float(data['data']['weather'][0]['hourly'][i+1][param]) # 1, 2, 3, 4
                 avg /= 4.0
                 return avg
             pass
 
-        def get_tomorrow(today, tomorrow):
+        #TODO: find if rain or snow. somehow. be smart
+        def get_tomorrow_precip(tomorrow):
+            tomorrow_precip = get_avg(tomorrow, 'precipMM') * 4.0
+            return tomorrow_precip
+
+        def get_tomorrow_night_precip(tomorrow):
+            tomorrow_night_precip = get_avg(tomorrow, 'precipMM', night=True) * 4.0
+            return tomorrow_night_precip
+
+        def get_tomorrow_night_temp(today, tomorrow):
+            today_min = today['data']['weather'][0]['mintempF']
+            tomorrow_min = tomorrow['data']['weather'][0]['mintempF']
+            return '(today_min - tomorrow_min): %f' %(float(today_min) - float(tomorrow_min))
+
+        def get_tomorrow_temp(today, tomorrow):
             # daytime
             today_max = today['data']['weather'][0]['maxtempF']
             tomorrow_max = tomorrow['data']['weather'][0]['maxtempF']
-
-            today_rain = get_avg(today, 'precipMM')
-            tomorrow_rain = get_avg(tomorrow, 'precipMM')
-
-            #TODO: nighttime?
-
-            return '(today_rain - tomorrow_rain): %f' %(float(today_rain) - float(tomorrow_rain))
             return '(today_max - tomorrow_max): %f' %(float(today_max) - float(tomorrow_max))
 
-        def get_today(yesterday, today):
-            # daytime
+
+        #TODO: find if rain or snow. somehow. be smart
+        def get_today_precip(today):
+            today_precip = get_avg(today, 'precipMM') * 4.0
+            return today_precip
+
+        def get_tonight_precip(today):
+            today_precip = get_avg(today, 'precipMM', night=True) * 4.0
+            return today_precip
+
+        def get_tonight_temp(yesterday, today):
+            today_min = today['data']['weather'][0]['mintempF']
+            yesterday_min = yesterday['data']['weather'][0]['mintempF']
+            return '(today_min - yesterday_min): %f' %(float(today_min) - float(yesterday_min))
+
+        def get_today_temp(yesterday, today):
             today_max = today['data']['weather'][0]['maxtempF']
             yesterday_max = yesterday['data']['weather'][0]['maxtempF']
-
-            today_rain = get_avg(today, 'precipMM')
-            yesterday_rain = get_avg(yesterday, 'precipMM')
-
-            #TODO: nighttime?
-
-            return '(today_rain - yesterday_rain): %f' %(float(today_rain) - float(yesterday_rain))
             return '(today_max - yesterday_max): %f' %(float(today_max) - float(yesterday_max))
 
         # Error checking and input validation
@@ -139,13 +153,19 @@ class API(webapp2.RequestHandler):
             #YESTERDAY - yesterday, midnight (AM)
             url = 'http://api.worldweatheronline.com/free/v2/past-weather.ashx?key='+key+'&format=json&q='+lat+','+lng+'&date='+yesterday_datetime.strftime('%Y-%m-%d')
             yesterday = json.loads(urllib2.urlopen(url).read())
-            response['today'] = get_today(yesterday, today)
+            response['today'] = get_today_temp(yesterday, today)
+            response['tonight'] = get_tonight_temp(yesterday, today)
+            response['today_precip'] = get_today_precip(today)
+            response['tonight_precip'] = get_tonight_precip(today)
 
         if include_tomorrow:
             #TOMORROW
             url = 'http://api.worldweatheronline.com/free/v2/weather.ashx?key='+key+'&format=json&q='+lat+','+lng+'&date='+tomorrow_datetime.strftime('%Y-%m-%d')
             tomorrow = json.loads(urllib2.urlopen(url).read())
-            response['tomorrow'] = get_tomorrow(today, tomorrow)
+            response['tomorrow'] = get_tomorrow_temp(today, tomorrow)
+            response['tomorrow_night'] = get_tomorrow_night_temp(today, tomorrow)
+            response['tomorrow_precip'] = get_tomorrow_precip(tomorrow)
+            response['tomorrow_night_precip'] = get_tomorrow_night_precip(tomorrow)
 
         #LOCATION
         url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&key=AIzaSyCGA86L8v4Lh-AUJHsKvQODP8SNsbTjYqg'
